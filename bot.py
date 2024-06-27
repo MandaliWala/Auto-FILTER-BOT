7b1d4693e168e3b3e23b7e38ea8f191a32f476fb
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-06-27 08:57:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,8 +61,9 @@ async def start():
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
     await Media.ensure_indexes()
+    await Media2.ensure_indexes()
     me = await TechVJBot.get_me()
-         await Media2.ensure_indexes()
+         
         #choose the right db by checking the free space
         stats = await clientDB.command('dbStats')
         #calculating the free db space from bytes to MB

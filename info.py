4741a72f663be49ac4fb8be53b00e4aa123395c4
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-19 17:18:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -109,7 +109,12 @@ SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 PORT = environ.get("PORT", "8080")
 MSG_ALRT = environ.get('MSG_ALRT', ' ᴅᴀᴛᴛᴇʙᴀʏᴏ ​!! ')
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", """{file_caption}
+🎬 Quality: {quality}
+⏳ Duration: {duration}
+💥 𝙅𝙊𝙄𝙉 : @MovieTimesTV
+💥 𝙊𝙒𝙉𝙀𝙍 : @Spider_Man_02
+""")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)

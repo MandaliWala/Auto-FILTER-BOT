a2ccab9c36701f07eee2b4f926156c2487dca717
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-07-23 19:09:19 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -24,7 +24,7 @@ async def song(client, message):
         query += ' ' + str(i)
     print(query)
     m = await message.reply(f"**ѕєαrchíng чσur ѕσng...!\n {query}**")
-    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    ydl_opts = {"format": "bestaudio[ext=mp3]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         link = f"https://youtube.com{results[0]['url_suffix']}"

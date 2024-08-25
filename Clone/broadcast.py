7a4d2ebd557a06b4f25424ae93c45c157588e108
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-08-25 15:30:03 +0530

    Update broadcast.py

--- Clone/broadcast.py
+++ Clone/broadcast.py
@@ -48,10 +48,6 @@ async def pm_broadcast(bot, message):
     except Exception as e:
         print(f"error: {e}")
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 async def broadcast_messages(bot_id, user_id, message):
     try:
         await message.copy(chat_id=user_id)

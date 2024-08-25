e2dec278a509461b73ef2daafd87517282db92d5
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2024-08-25 15:29:44 +0530

    Update broadcast.py

--- Clone/broadcast.py
+++ Clone/broadcast.py
@@ -1,9 +1,9 @@
 import datetime, time, asyncio
 from pyrogram import Client, filters
 from database.users_chats_db import db
+from pyrogram.errors import *
 from CloneTechVJ.database.clone_bot_userdb import clonedb
-from utils import broadcast_messages
-
+        
 @Client.on_message(filters.command("broadcast"))
 async def pm_broadcast(bot, message):
     me = await bot.get_me()
@@ -12,7 +12,7 @@ async def pm_broadcast(bot, message):
         return 
     b_msg = await bot.ask(chat_id = message.from_user.id, text = "Now Send Me Your Broadcast Message")
     try:
-        users = await clonedb.get_all_users()
+        users = await clonedb.get_all_users(me.id)
         sts = await message.reply_text('Broadcasting your messages...')
         start_time = time.time()
         total_users = await clonedb.total_users_count(me.id)
@@ -22,8 +22,8 @@ async def pm_broadcast(bot, message):
         failed = 0
         success = 0
         async for user in users:
-            if 'id' in user:
-                pti, sh = await broadcast_messages(int(user['id']), b_msg)
+            if 'user_id' in user:
+                pti, sh = await broadcast_messages(me.id, int(user['user_id']), b_msg)
                 if pti:
                     success += 1
                 elif pti == False:
@@ -42,8 +42,32 @@ async def pm_broadcast(bot, message):
                 failed += 1
                 if not done % 20:
                     await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-
+    
         time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
         await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users: {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
     except Exception as e:
         print(f"error: {e}")
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+async def broadcast_messages(bot_id, user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return True, "Success"
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return await broadcast_messages(bot_id, user_id, message)
+    except InputUserDeactivated:
+        await clonedb.delete_user(bot_id, user_id)
+        return False, "Deleted"
+    except UserIsBlocked:
+        await clonedb.delete_user(bot_id, user_id)
+        return False, "Blocked"
+    except PeerIdInvalid:
+        await clonedb.delete_user(bot_id, user_id)
+        return False, "Error"
+    except Exception as e:
+        await clonedb.delete_user(bot_id, user_id)
+        return False, "Error"

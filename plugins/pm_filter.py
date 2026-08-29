abdb446086409d41fcb7907628a6785864cb65ac
Naman <179840224+NamanTG@users.noreply.github.com>
2026-08-29 20:18:27 +0530

    bruh

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -792,7 +792,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             LOGGER.error(e)
                     
     elif query.data == "me":
-        [
+        buttons = [
             InlineKeyboardButton('⇋ ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ ⇋', callback_data='start')
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
